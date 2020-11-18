f3d79156694ef2b2bdf4b2de25560fb8d71ab0fb
DELL <3088188994@qq.com>
2020-11-18 20:27:06 +0800

    第一次提交

---
+++ gmall-user-manage/src/main/java/com/galaxy/gmall/usermanage/mapper/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.galaxy.gmall.usermanage.mapper;
+
+import com.galaxy.gmall.usermanage.bean.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserInfoMapper extends Mapper<UserInfo> {
+}
