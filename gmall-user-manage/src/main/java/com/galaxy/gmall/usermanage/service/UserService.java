ad1084970cf9e14acdbd1ef6c13c1693333bc562
DELL <3088188994@qq.com>
2020-11-18 20:36:12 +0800

    第一次提交

---
+++ gmall-user-manage/src/main/java/com/galaxy/gmall/usermanage/service/UserService.java
@@ -0,0 +1,19 @@
+package com.galaxy.gmall.usermanage.service;
+
+import com.galaxy.gmall.usermanage.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserInfo> getUserInfoListAll();
+
+    void addUser(UserInfo userInfo);
+
+    void updateUser(UserInfo userInfo);
+
+    void updateUserByName(String name,UserInfo userInfo);
+
+    void delUser(UserInfo userInfo);
+
+}
